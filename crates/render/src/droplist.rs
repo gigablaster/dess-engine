7d19508332efd506c414b7c93d92c2d7df6a980c
gigablaster <gigakek@protonmail.com>
2023-05-13 10:30:44 +0500

    Added default capacity for drop list

--- crates/render/src/droplist.rs
+++ crates/render/src/droplist.rs
@@ -3,13 +3,27 @@ use gpu_alloc_ash::AshMemoryDevice;
 
 use crate::{Allocation, Allocator};
 
-#[derive(Debug, Default)]
+const CAPACITY: usize = 32;
+
+#[derive(Debug)]
 pub(crate) struct DropList {
     memory_to_free: Vec<Allocation>,
     images_to_free: Vec<vk::Image>,
     image_views_to_free: Vec<vk::ImageView>,
     buffers_to_free: Vec<vk::Buffer>,
     buffer_views_to_free: Vec<vk::BufferView>,
+}
+
+impl Default for DropList {
+    fn default() -> Self {
+        Self {
+            memory_to_free: Vec::with_capacity(CAPACITY * 2),
+            images_to_free: Vec::with_capacity(CAPACITY),
+            image_views_to_free: Vec::with_capacity(CAPACITY),
+            buffers_to_free: Vec::with_capacity(CAPACITY),
+            buffer_views_to_free: Vec::with_capacity(CAPACITY),
+        }
+    }
 }
 
 impl DropList {
@@ -50,5 +64,10 @@ impl DropList {
         self.memory_to_free.drain(..).for_each(|mem| {
             unsafe { allocator.dealloc(device, mem) };
         });
+        self.image_views_to_free.shrink_to(CAPACITY);
+        self.buffer_views_to_free.shrink_to(CAPACITY);
+        self.images_to_free.shrink_to(CAPACITY);
+        self.buffers_to_free.shrink_to(CAPACITY);
+        self.memory_to_free.shrink_to(CAPACITY * 2);
     }
 }
