16b7a83661a41beb12ba6cd9f5f287fefdfba3f1
gigablaster <gigakek@protonmail.com>
2024-01-02 18:22:58 +0500

    Use new model for testing

--- crates/lib/runner/examples/demo.rs
+++ crates/lib/runner/examples/demo.rs
@@ -13,7 +13,6 @@ use dess_engine::{
 };
 use dess_runner::{Client, InitContext, RenderContext, Runner, UpdateContext};
 use glam::vec3;
-use parking_lot::Mutex;
 
 #[derive(Default)]
 struct ClearBackbuffer<'a> {
@@ -71,40 +70,41 @@ struct SceneUniform {
 }
 
 impl<'a> ClearBackbuffer<'a> {
-    fn create_draw_stream(&self, context: &RenderContext<'a>, x: f32, y: f32) -> DrawStream {
+    fn create_draw_stream(&self, context: &RenderContext<'a>, rotation: f32) -> DrawStream {
         puffin::profile_function!();
         let mut stream = DrawStream::new(self.scene_bind_group);
-        for z in -80..80 {
-            stream.set_pipeline(self.pipeline);
-            for model in self.model.models.values() {
-                let mut bones = Vec::with_capacity(model.bones.len());
-                for (index, bone) in model.bones.iter().enumerate() {
-                    let parent = model.bone_parents[index];
-                    if parent == u32::MAX {
-                        bones.push(
-                            glam::Mat4::from_translation(vec3(x * 0.1, y * 0.1, z as f32 * 0.1))
-                                * glam::Mat4::from(*bone),
-                        );
-                    } else {
-                        bones.push(bones[parent as usize] * glam::Mat4::from(*bone))
-                    }
+        stream.set_pipeline(self.pipeline);
+        for model in self.model.models.values() {
+            let mut bones = Vec::with_capacity(model.bones.len());
+            for (index, bone) in model.bones.iter().enumerate() {
+                let parent = model.bone_parents[index];
+                if parent == u32::MAX {
+                    bones.push(glam::Mat4::from_rotation_z(rotation) * glam::Mat4::from(*bone));
+                } else {
+                    bones.push(bones[parent as usize] * glam::Mat4::from(*bone))
                 }
-                let temp = context.frame.temp_allocate(&bones).unwrap();
-                stream.set_dynamic_buffer_offset(0, Some(temp.offset));
-                stream.set_bind_group(3, Some(self.draw_bind_group));
-                for (bone_idx, mesh_idx) in &model.instances {
-                    let mesh = &model.static_meshes[*mesh_idx as usize];
-                    stream.set_vertex_buffer(0, Some(mesh.vertices));
-                    stream.set_vertex_buffer(1, Some(mesh.attributes));
-                    stream.set_index_buffer(Some(mesh.indices));
-                    for submesh in &mesh.submeshes {
-                        stream.set_bind_group(
-                            1,
-                            Some(mesh.resolved_materials[submesh.material_index].main_bind_group),
-                        );
-                        stream.set_bind_group(2, Some(submesh.object_bind_group));
-                        stream.draw(submesh.first_index, 0, submesh.index_count, 1, *bone_idx);
-                    }
+            }
+            let temp = context.frame.temp_allocate(&bones).unwrap();
+            stream.set_dynamic_buffer_offset(0, Some(temp.offset));
+            stream.set_bind_group(3, Some(self.draw_bind_group));
+            for (bone_idx, mesh_idx) in &model.instances {
+                let mesh = &model.static_meshes[*mesh_idx as usize];
+                stream.set_vertex_buffer(0, Some(mesh.vertices));
+                stream.set_vertex_buffer(1, Some(mesh.attributes));
+                stream.set_index_buffer(Some(mesh.indices));
+                for submesh in &mesh.submeshes {
+                    stream.set_bind_group(
+                        1,
+                        Some(mesh.resolved_materials[submesh.material_index].main_bind_group),
+                    );
+                    stream.set_bind_group(2, Some(submesh.object_bind_group));
+                    stream.draw(
+                        submesh.first_index,
+                        submesh.vertex_offset,
+                        submesh.index_count,
+                        1,
+                        *bone_idx,
+                    );
                 }
             }
         }
@@ -138,7 +138,7 @@ impl<'a> Client for ClearBackbuffer<'a> {
             ImageLayout::ColorTarget,
         )
         .store_output()
-        .clear_input(ClearRenderTarget::Color([0.125, 0.25, 0.5, 1.0]));
+        .clear_input(ClearRenderTarget::Color([0.125, 0.125, 0.125, 1.0]));
         let depth_target =
             RenderTarget::new(Format::D24, depth.view(), ImageLayout::DepthStencilTarget)
                 .clear_input(ClearRenderTarget::DepthStencil(1.0, 0));
@@ -147,7 +147,7 @@ impl<'a> Client for ClearBackbuffer<'a> {
             .with_bind_groups(|ctx| {
                 let scene = SceneUniform {
                     view: glam::Mat4::look_at_rh(
-                        vec3(0.1, 0.1, 0.1),
+                        vec3(0.0, 0.25, 0.2),
                         glam::Vec3::ZERO,
                         glam::Vec3::Z,
                     ),
@@ -163,29 +163,12 @@ impl<'a> Client for ClearBackbuffer<'a> {
                 Ok(())
             })
             .unwrap();
-        let streams = Arc::new(Mutex::new(vec![Option::<DrawStream>::None; 20 * 20]));
-        {
-            puffin::profile_scope!("Generate draw calls");
-            rayon::scope(|s| {
-                for x in 0..20 {
-                    for y in 0..20 {
-                        let context = &context;
-                        let streams = streams.clone();
-                        s.spawn(move |_| {
-                            let index = x + y * 20;
-                            let stream =
-                                self.create_draw_stream(context, (x - 10) as f32, (y - 10) as f32);
-                            streams.lock()[index as usize] = Some(stream);
-                        })
-                    }
-                }
-            });
-        }
+        let stream = self.create_draw_stream(&context, 0.0);
         context.frame.execute(
             context.frame.render_area,
             &[color_target],
             Some(depth_target),
-            streams.lock().drain(..).flatten(),
+            [stream].into_iter(),
             &[],
         );
 
@@ -219,7 +202,7 @@ impl<'a> Client for ClearBackbuffer<'a> {
             .unwrap();
         let model = context
             .resource_manager
-            .request_model(GltfSource::new("models/Avocado/Avocado.gltf").get_ref());
+            .request_model(GltfSource::new("models/PBR/ABeautifulGame.gltf").get_ref());
         context
             .resource_manager
             .with_context(|ctx| {
